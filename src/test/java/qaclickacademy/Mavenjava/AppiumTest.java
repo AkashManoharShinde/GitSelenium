047240034d35c6aeef32267ccde67d5cb238a55e
Akash <akashshinde723@gmail.com>
2021-03-22 18:53:38 +0530

    first commit

---
+++ src/test/java/qaclickacademy/Mavenjava/AppiumTest.java
@@ -0,0 +1,17 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+@Test
+public void appium1()
+{
+	System.out.println("appium1");
+}
+
+@Test
+public void appium2()
+{
+	System.out.println("appium2");
+}
+}
---
+++ src/test/java/qaclickacademy/Mavenjava/RestAPITest.java
@@ -0,0 +1,17 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class RestAPITest {
+	@Test
+	public void RestAPITest1()
+	{
+		System.out.println("RestAPITest1");
+	}
+
+	@Test
+	public void RestAPITest2()
+	{
+		System.out.println("RestAPITest2");
+	}
+}
